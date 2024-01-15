7fe23d0087440661b348fcb1eeb422239b6e688a
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-01-15 14:49:34 +0800

    默认 bean

---
+++ cloud-core/cloud-core-mysql/src/main/java/cc/xiaoxu/cloud/core/bean/dto/PageDTO.java
@@ -0,0 +1,25 @@
+package cc.xiaoxu.cloud.core.bean.dto;
+
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+@ToString
+@Data
+public class PageDTO {
+
+    @Schema(description = "页码", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long current;
+
+    @Schema(description = "每页数量", example = "10", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long size;
+
+    /**
+     * 排序 传入排序字段以及正序倒序
+     */
+    @Schema(title = "排序")
+    private List<OrderItem> orders;
+}
