7e6e151d5a59397c4df8008d67bbd996593f3767
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-01-27 10:12:17 +0800

    更新工具类

--- cloud-core/cloud-core-utils/src/main/java/cc/xiaoxu/cloud/core/utils/text/MD5Utils.java
+++ cloud-core/cloud-core-utils/src/main/java/cc/xiaoxu/cloud/core/utils/text/MD5Utils.java
@@ -1,6 +1,11 @@
 package cc.xiaoxu.cloud.core.utils.text;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -50,5 +55,38 @@ public class MD5Utils {
         } catch (Exception ignored) {
         }
         return null;
+    }
+
+    /**
+     * 计算文件 MD5
+     *
+     * @param file 文件
+     * @return 文件 MD5
+     */
+    public static String toMd5(File file) {
+        try (InputStream stream = Files.newInputStream(file.toPath(), StandardOpenOption.READ)) {
+            return toMd5(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static String toMd5(InputStream stream) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            byte[] buf = new byte[8192];
+            int len;
+            while ((len = stream.read(buf)) > 0) {
+                digest.update(buf, 0, len);
+            }
+            return toHexString(digest.digest());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
