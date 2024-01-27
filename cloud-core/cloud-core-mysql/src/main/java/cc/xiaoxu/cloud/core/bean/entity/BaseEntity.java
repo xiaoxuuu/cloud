677be1bfb4932057890021ce86a45da8cec5b684
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-01-27 10:13:20 +0800

    枚举位置调整

--- cloud-core/cloud-core-mysql/src/main/java/cc/xiaoxu/cloud/core/bean/entity/BaseEntity.java
+++ cloud-core/cloud-core-mysql/src/main/java/cc/xiaoxu/cloud/core/bean/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package cc.xiaoxu.cloud.core.bean.entity;
 
+import cc.xiaoxu.cloud.bean.enums.StateEnum;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
