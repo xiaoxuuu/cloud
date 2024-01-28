cf00859822552b82462c44c1c90a795de613a92e
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-01-28 10:13:52 +0800

    添加返回类

---
+++ cloud-core/cloud-core-spring/src/main/java/cc/xiaoxu/cloud/bean/enums/StateEnum.java
@@ -0,0 +1,21 @@
+package cc.xiaoxu.cloud.bean.enums;
+
+import cc.xiaoxu.cloud.core.utils.enums.EnumInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum StateEnum implements EnumInterface<String> {
+
+    /**
+     * 启用 E；删除 D；禁用 L；审核中 T；
+     */
+    E("E", "启用"),
+    D("D", "删除"),
+    L("L", "禁用"),
+    T("T", "审核");
+
+    private final String code;
+    private final String introduction;
+}
