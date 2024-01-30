9ca098042e08bc00bb3626b7341343e4c705eaf7
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-01-30 10:38:46 +0800

    包引用

--- cloud-single/src/main/java/cc/xiaoxu/cloud/handler/RequestUrlFilter.java
+++ cloud-single/src/main/java/cc/xiaoxu/cloud/handler/RequestUrlFilter.java
@@ -1,10 +1,10 @@
 package cc.xiaoxu.cloud.handler;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
