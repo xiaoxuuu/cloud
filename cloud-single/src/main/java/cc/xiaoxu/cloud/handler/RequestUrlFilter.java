866bba81fe22552cfc199e00fc7df6897be0a3b2
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-01-30 10:36:18 +0800

    single 模块

---
+++ cloud-single/src/main/java/cc/xiaoxu/cloud/handler/RequestUrlFilter.java
@@ -0,0 +1,74 @@
+package cc.xiaoxu.cloud.handler;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class RequestUrlFilter implements Filter {
+
+    private static final Set<String> REPLACE_SET = new HashSet<>();
+
+    static {
+        REPLACE_SET.add("/authorize");
+        REPLACE_SET.add("/service_portrait");
+        REPLACE_SET.add("/service_system");
+        REPLACE_SET.add("/service_file");
+        REPLACE_SET.add("/service_ideology");
+    }
+
+    private String handle(String uri) {
+
+        for (String s : REPLACE_SET) {
+            if (uri.startsWith(s)) {
+                return uri.replace(s, "");
+            }
+        }
+        return uri;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String requestUri = req.getRequestURI();
+        StringBuffer requestUrl = req.getRequestURL();
+        // 将请求 URI 修改为要修改的 URI
+        String modifiedUri = handle(requestUri);
+        String modifiedUrl = requestUrl.toString().replace(requestUri, modifiedUri);
+
+        // 创建一个包含修改后 URI 的请求对象
+        ModifiedUriHttpServletRequestWrapper wrapper = new ModifiedUriHttpServletRequestWrapper(req, modifiedUri, modifiedUrl);
+
+        // 将修改后的请求对象传递给下一个过滤器
+        chain.doFilter(wrapper, response);
+    }
+}
+
+class ModifiedUriHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+    private final String modifiedUri;
+    private final String modifiedUrl;
+
+    public ModifiedUriHttpServletRequestWrapper(HttpServletRequest request, String modifiedUri, String modifiedUrl) {
+        super(request);
+        this.modifiedUri = modifiedUri;
+        this.modifiedUrl = modifiedUrl;
+    }
+
+    @Override
+    public String getRequestURI() {
+        return modifiedUri;
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        return new StringBuffer(modifiedUrl);
+    }
+}
