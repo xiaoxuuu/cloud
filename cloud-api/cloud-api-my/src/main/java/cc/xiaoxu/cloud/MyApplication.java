c3a8a90813402f35493a0ee7adbd4a630307086f
小徐 <xuhaoran@live.com>
2024-02-03 17:04:56 +0800

    引入 MY 模块

---
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/MyApplication.java
@@ -0,0 +1,27 @@
+package cc.xiaoxu.cloud;
+
+import cc.xiaoxu.cloud.core.utils.GetStartInfoUtils;
+import cc.xiaoxu.cloud.my.navigation.task.WebsiteCheckTask;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = "cc.xiaoxu.cloud")
+@MapperScan(basePackages = {"cc.xiaoxu.cloud.my.navigation.dao"})
+public class MyApplication {
+
+    public static void main(String[] args) {
+
+        // 获取 Spring Boot 上下文
+        ConfigurableApplicationContext ctx = SpringApplication.run(MyApplication.class, args);
+        log.error(GetStartInfoUtils.getLog(ctx));
+        // 手动执行定时任务
+        WebsiteCheckTask task = ctx.getBean(WebsiteCheckTask.class);
+        task.refreshData();
+    }
+}
---
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/navigation/service/NavWebsiteIconService.java
@@ -0,0 +1,43 @@
+package cc.xiaoxu.cloud.my.navigation.service;
+
+import cc.xiaoxu.cloud.core.bean.enums.StateEnum;
+import cc.xiaoxu.cloud.my.navigation.bean.entity.NavWebsiteIcon;
+import cc.xiaoxu.cloud.my.navigation.dao.NavWebsiteIconMapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class NavWebsiteIconService extends ServiceImpl<NavWebsiteIconMapper, NavWebsiteIcon> {
+
+    /**
+     * 数据缓存
+     */
+    private Map<String, NavWebsiteIcon> navIconMap;
+
+    public Map<String, NavWebsiteIcon> getNavIconMap() {
+        return navIconMap;
+    }
+
+    public void setNavIconMap(List<NavWebsiteIcon> navIconList) {
+        this.navIconMap = navIconList.stream().collect(Collectors.toMap(NavWebsiteIcon::getId, a -> a));
+    }
+
+    /**
+     * 查询全量数据
+     */
+    public List<NavWebsiteIcon> getList() {
+
+        LambdaQueryWrapper<NavWebsiteIcon> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(NavWebsiteIcon::getState, StateEnum.E.getCode());
+        List<NavWebsiteIcon> list = this.list(queryWrapper);
+        log.debug("查询到 {} 条已知网站图标...", list.size());
+        return list;
+    }
+}
