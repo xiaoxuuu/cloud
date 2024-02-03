0f736b2c09835a706fe86f7280ea7a46709dd2cf
小徐 <xuhaoran@live.com>
2024-02-03 10:34:55 +0800

    工具添加

---
+++ cloud-core/cloud-core-spring/src/main/java/cc/xiaoxu/cloud/core/controller/InfoController.java
@@ -0,0 +1,52 @@
+package cc.xiaoxu.cloud.core.controller;
+
+import cc.xiaoxu.cloud.core.utils.DateUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 2022/5/11 10:33
+ *
+ * @author Xiao Xu
+ */
+@RestController
+@Tag(name = "系统信息", description = "core 内置接口，用于查询模块信息，依赖 core 的服务均自带此接口")
+@RequestMapping("/info")
+public class InfoController {
+
+    @Value("${time.package:未读取到配置}")
+    private String packageTime;
+
+    @Value("${time.start:未读取到配置}")
+    private String startTime;
+
+    @GetMapping("/get")
+    @Operation(summary = "读取项目基本信息", description = "可展示模块以下信息：打包时间、启动时间、主机名、服务ip、域名")
+    public String get() {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("打包时间", packageTime.replace("_", " "));
+        map.put("启动时间", startTime.replace("_", " "));
+        map.put("主机时间", DateUtils.getNowTime());
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            map.put("主机名", inetAddress.getHostName());
+            map.put("服务IP", inetAddress.getHostAddress());
+            map.put("域名", inetAddress.getCanonicalHostName());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+
+        return map.entrySet().stream().map(k -> k.getKey() + "：" + k.getValue()).collect(Collectors.joining("，"));
+    }
+}
---
+++ cloud-core/cloud-core-spring/src/main/java/cc/xiaoxu/cloud/core/handler/GlobalExceptionHandler.java
@@ -0,0 +1,64 @@
+package cc.xiaoxu.cloud.core.handler;
+
+import cc.xiaoxu.cloud.core.bean.enums.REnum;
+import cc.xiaoxu.cloud.core.bean.vo.R;
+import cc.xiaoxu.cloud.core.exception.CustomException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 运行时异常
+     *
+     * @param e 异常
+     * @return message
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public R<Void> handle(RuntimeException e) {
+
+        log.error("运行时异常拦截: {}", e.getMessage());
+        e.printStackTrace();
+        return R.fail(REnum.RUNTIME_EXCEPTION, e);
+    }
+
+    /**
+     * 自定义异常
+     *
+     * @param e 异常
+     * @return message
+     */
+    @ExceptionHandler(value = CustomException.class)
+    public R<String> handleCustom(CustomException e) {
+
+        log.error("自定义异常拦截: {}", e.getMessage());
+        e.printStackTrace();
+        return e.getResult();
+    }
+
+    /**
+     * 参数校验异常拦截
+     *
+     * @param e 参数异常
+     * @return message
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public R<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+
+        // 从异常对象中拿到 ObjectError 对象
+        ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
+        log.error("参数校验异常拦截: {}", objectError.getDefaultMessage());
+        // 然后提取错误提示信息进行返回
+        return R.fail(objectError);
+    }
+}
