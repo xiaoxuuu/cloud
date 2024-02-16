313b966836d9414194d92d432849d71b8fb448e6
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-02-16 11:01:26 +0800

    更新工具

--- cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/navigation/utils/WebsiteUtil.java
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/navigation/utils/WebsiteUtil.java
@@ -1,7 +1,5 @@
 package cc.xiaoxu.cloud.my.navigation.utils;
 
-import cc.xiaoxu.cloud.core.exception.CustomException;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,35 +12,9 @@ import java.util.regex.Pattern;
 
 public class WebsiteUtil {
 
+    // 处理 301 问题
     public static void main(String[] args) {
-        System.out.println("【" + getWebsiteTitle("https://xiaoxu.cc/") + "】");
-    }
-
-    public static String getWebsiteTitle(String url) {
-
-        String title = "";
-        // 要获取标题的网站 URL
-        try {
-            URI uri = URI.create(url);
-            URL website = uri.toURL();
-            URLConnection connection = website.openConnection();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.contains("<title>")) {
-                    int startIndex = line.indexOf("<title>") + 7;
-                    int endIndex = line.indexOf("</title>");
-                    title = line.substring(startIndex, endIndex);
-                    break;
-                }
-            }
-            reader.close();
-        } catch (IOException e) {
-            throw new CustomException(e.getLocalizedMessage());
-        }
-        return title;
+        System.out.println("【" + getTitle("https://xiaoxu.cc/") + "】");
     }
 
     /**
@@ -82,7 +54,8 @@ public class WebsiteUtil {
         URI uri = URI.create(httpUrl);
         URL url = uri.toURL();
         // 获得当前 URL 的字节流（缓冲）
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+        URLConnection connection = url.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
         String input;
         // 当前行存在数据时
         while ((input = reader.readLine()) != null) {
