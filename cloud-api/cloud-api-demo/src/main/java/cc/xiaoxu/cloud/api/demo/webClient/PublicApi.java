828601267f6c5850eb1e32a3d6de2fb3c20b52e8
小徐 <xuhaoran@live.com>
2024-02-22 13:45:43 +0800

    对接

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/webClient/PublicApi.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/webClient/PublicApi.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.service.annotation.PostExchange;
 
-//@HttpExchange("")
 public interface PublicApi {
 
     @PostExchange("/gsxt/PlatformSHZZFRKGSXT/biz/ma/shzzgsxt/a/getAae01CertificateInfo.html")
---
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/webClient/ResultDetail.java
@@ -0,0 +1,28 @@
+package cc.xiaoxu.cloud.api.demo.webClient;
+
+import lombok.Data;
+
+@Data
+public class ResultDetail {
+
+    private String aaae0105;
+    private String aaae0116;
+    private String aaae0106;
+    private String aaae0103;
+    private String aaae0114;
+    private String aaae0703;
+    private String aaae0109;
+    private String aaae0704;
+    private String aaae0107;
+    private int aaae0129;
+    private String time;
+    private String aaae0101;
+    private String aaae0123;
+    private String aaae0145;
+    private String aaae0102;
+    private String aaae0113;
+    private String aaae9995;
+    private String aaae0110;
+    private String aaae0122;
+    private String aaae0111;
+}
