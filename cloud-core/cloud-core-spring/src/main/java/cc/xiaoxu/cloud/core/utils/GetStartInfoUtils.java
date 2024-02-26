ff419ec16475fe28724d22e2929c0555c1e2d45e
小徐 <xuhaoran@live.com>
2024-02-26 10:54:52 +0800

    banner

--- cloud-core/cloud-core-spring/src/main/java/cc/xiaoxu/cloud/core/utils/GetStartInfoUtils.java
+++ cloud-core/cloud-core-spring/src/main/java/cc/xiaoxu/cloud/core/utils/GetStartInfoUtils.java
@@ -14,25 +14,23 @@ public class GetStartInfoUtils {
 
     public static String getLog(ConfigurableApplicationContext ctx) {
 
+        String notConfigured = "Not Configured";
         ConfigurableEnvironment environment = ctx.getEnvironment();
         // 应用名
         String name = environment.getProperty("spring.application.name");
-        name = StringUtils.isBlank(name) ? "未读取到配置" : name.toUpperCase();
+        name = StringUtils.isBlank(name) ? notConfigured : name.toUpperCase();
         // 端口
         String port = environment.getProperty("server.port");
-        port = StringUtils.isBlank(port) ? "未读取到配置" : port;
-        // 版本
-        String appVersion = environment.getProperty("app.version");
-        appVersion = StringUtils.isBlank(appVersion) ? "" : appVersion;
+        port = StringUtils.isBlank(port) ? notConfigured : port;
         // 打包时间
         String packageTime = environment.getProperty("time.package");
-        packageTime = StringUtils.isBlank(packageTime) ? "未读取到配置" : packageTime.replace("_", " ");
+        packageTime = StringUtils.isBlank(packageTime) ? notConfigured : packageTime.replace("_", " ");
         // 启动时间
         String startTime = environment.getProperty("time.start");
-        startTime = StringUtils.isBlank(startTime) ? "未读取到配置" : startTime.replace("_", " ");
+        startTime = StringUtils.isBlank(startTime) ? notConfigured : startTime.replace("_", " ");
 
         // 文本处理
-        String message = "%s start success. Port: %s, Version: %s, PackageTime: %s, StartTime: %s";
-        return message.formatted(name, port, appVersion, packageTime, startTime);
+        String message = "%s(%s) Start Success, Package Time: %s, Start Time: %s";
+        return message.formatted(name, port, packageTime, startTime);
     }
 }
