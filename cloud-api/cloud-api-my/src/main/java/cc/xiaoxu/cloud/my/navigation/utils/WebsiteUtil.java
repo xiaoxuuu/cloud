6693ede7d11277e202bb992d7d85983bebaa7ddf
小徐 <xuhaoran@live.com>
2024-03-01 10:03:02 +0800

    超时时间

--- cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/navigation/utils/WebsiteUtil.java
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/navigation/utils/WebsiteUtil.java
@@ -59,7 +59,7 @@ public class WebsiteUtil {
         // 获得当前 URL 的字节流（缓冲）
         URLConnection connection = url.openConnection();
         // 设置超时时间
-        connection.setReadTimeout(5 * 1000);
+        connection.setReadTimeout(500);
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
         String input;
         // 当前行存在数据时
