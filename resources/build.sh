f351b460f0b793f8731cdecaca1d9d5b9ed908ff
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-03-08 14:09:26 +0800

    关闭正常 mvn 日志

--- resources/build.sh
+++ resources/build.sh
@@ -2,4 +2,6 @@
 # 作者：小徐
 # 时间：2024-02-26
 
-mvn -DsendCredentialsOverHttp=true -Dmaven.test.skip=true clean install -P local,releases -T 12
+echo "mvn deploy start"
+mvn -DsendCredentialsOverHttp=true -Dmaven.test.skip=true clean install -P local,releases -T 12 -q
+echo "mvn deploy finish"
