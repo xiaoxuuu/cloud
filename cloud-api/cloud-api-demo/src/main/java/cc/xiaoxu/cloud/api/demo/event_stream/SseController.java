4ee2c9adf8f2e787e17291ecdf10f812f126a199
小徐 <xuhaoran@live.com>
2024-03-17 11:34:08 +0800

    SSE 调整

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/event_stream/SseController.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/event_stream/SseController.java
@@ -1,51 +1,55 @@
 package cc.xiaoxu.cloud.api.demo.event_stream;
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-@RestController("/sse")
+@RestController
+@RequestMapping("/sse")
 public class SseController {
 
     private final CopyOnWriteArrayList<SseEmitter> emitterList = new CopyOnWriteArrayList<>();
 
-    @GetMapping(value = "/events", produces = "text/event-stream")
+    @PostMapping(value = "/events", produces = "text/event-stream")
+//    @GetMapping(value = "/events", produces = "text/event-stream")
     public SseEmitter events(HttpServletResponse response) throws IOException {
 
         // 设置响应的字符编码为 UTF-8
         response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-Type", "text/event-steam");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Connection", "keep-alive");
 
         SseEmitter emitter = new SseEmitter();
         // 保持对 SseEmitter 的引用，以便后续发送消息
         emitterList.add(emitter);
 
-        // 发送初始消息
-        emitter.send("初始化消息");
+        String conversationId = "181";
 
         // 定时发送消息
         new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    // 每秒发送一次消息
-                    Thread.sleep(1000);
-                    emitter.send("消息 " + (i + 1));
-                } catch (InterruptedException | IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            // 发送完成消息并关闭连接
             try {
-                emitter.send("完成");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            emitter.complete();
-        }).start();
+                emitter.send(new JsonResult<>("START", null));
+                emitter.send(new JsonResult<>("ID", 1));
+                emitter.send(new JsonResult<>("NAME", "你好"));
+                for (int i = 0; i < 20; i++) {
 
+                    // 每秒发送一次消息
+                    Thread.sleep(100);
+                    emitter.send(new JsonResult<>("MSG", "" + (i + 1)));
+
+                }
+                emitter.send(new JsonResult<>("END", null));
+            } catch (Exception ignored) {
+            } finally {
+                emitter.complete();
+            }
+        }).start();
         return emitter;
     }
 
