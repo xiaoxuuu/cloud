b7c5157d4712d585a710a8dd402cf4097991cfeb
小徐 <xuhaoran@live.com>
2024-03-23 13:43:06 +0800

    gateway

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/DemoApplication.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/DemoApplication.java
@@ -14,7 +14,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 
-        // 获取 Spring Boot 上下文
         ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
         log.error(GetStartInfoUtils.getLog(ctx));
     }
