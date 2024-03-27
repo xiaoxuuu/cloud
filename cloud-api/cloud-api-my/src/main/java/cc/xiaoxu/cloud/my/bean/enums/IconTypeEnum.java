ea18f3ba0c57b87ed70869305c557759298239b3
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-03-27 14:39:36 +0800

    目录调整

---
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/bean/enums/IconTypeEnum.java
@@ -0,0 +1,21 @@
+package cc.xiaoxu.cloud.my.bean.enums;
+
+import cc.xiaoxu.cloud.core.utils.enums.EnumInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum IconTypeEnum implements EnumInterface<String> {
+
+    /**
+     *
+     */
+    SVG("S", ""),
+    BASE64("B", ""),
+    NULL("N", ""),
+    ;
+
+    private final String code;
+    private final String introduction;
+}
---
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/bean/vo/NavWebsiteShowVO.java
@@ -0,0 +1,44 @@
+package cc.xiaoxu.cloud.my.bean.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NavWebsiteShowVO {
+
+    @Schema(description = "id")
+    private String id;
+
+    @Schema(description = "展示名称")
+    private String shortName;
+
+    @Schema(description = "链接")
+    private String url;
+
+    @Schema(description = "网站描述")
+    private String description;
+
+    @Schema(description = "标签")
+    private Set<String> labelSet;
+
+    @Schema(description = "类型")
+    private Set<String> typeSet;
+
+    @Schema(description = "图标")
+    private String icon;
+
+    @Schema(description = "图标类型：BASE64, SVG")
+    private String iconType;
+
+    @Schema(description = "访问次数")
+    private Integer visitNum;
+
+    @Schema(description = "上次访问时间")
+    private String lastVisitDesc;
+}
