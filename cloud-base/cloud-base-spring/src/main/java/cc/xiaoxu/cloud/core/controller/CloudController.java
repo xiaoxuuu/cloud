33232a2765053868b0ea4b146d8af4b20a9e4215
小徐 <xuhaoran@live.com>
2024-04-08 14:10:44 +0800

    重命名

--- cloud-base/cloud-base-spring/src/main/java/cc/xiaoxu/cloud/core/controller/CloudController.java
+++ cloud-base/cloud-base-spring/src/main/java/cc/xiaoxu/cloud/core/controller/CloudController.java
@@ -21,8 +21,8 @@ import java.util.stream.Collectors;
  */
 @RestController
 @Tag(name = "系统信息", description = "core 内置接口，用于查询模块信息，依赖 core 的服务均自带此接口")
-@RequestMapping("/info")
-public class InfoController {
+@RequestMapping("/cloud")
+public class CloudController {
 
     @Value("${time.package:未读取到配置}")
     private String packageTime;
@@ -30,7 +30,7 @@ public class InfoController {
     @Value("${time.start:未读取到配置}")
     private String startTime;
 
-    @GetMapping("/get")
+    @GetMapping("/info")
     @Operation(summary = "读取项目基本信息", description = "可展示模块以下信息：打包时间、启动时间、主机名、服务ip、域名")
     public String get() {
 
