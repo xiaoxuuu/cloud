7ac944a696c0bb514961acc89c35e9d74a461796
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-04-11 14:19:55 +0800

    ConditionUtils DEMO

---
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/condition_demo/ConditionDemo.java
@@ -0,0 +1,15 @@
+package cc.xiaoxu.cloud.api.demo.condition_demo;
+
+import cc.xiaoxu.cloud.core.utils.ConditionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class ConditionDemo {
+
+    public static void main(String[] args) {
+
+        String a = "1";
+        ConditionUtils.of(a, StringUtils::isNotBlank).handle(System.out::println);
+        System.out.println(ConditionUtils.of(a, StringUtils::isNotBlank).get());
+        ConditionUtils.of(a, StringUtils::isNotBlank).handle(k -> System.out.println(123), System.out::println);
+    }
+}
