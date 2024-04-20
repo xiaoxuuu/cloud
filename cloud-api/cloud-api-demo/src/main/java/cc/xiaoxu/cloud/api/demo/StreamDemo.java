4d12eace1d975703da41b66363f69093a674b3c1
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-04-20 22:18:48 +0800

    函数调整

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/StreamDemo.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/StreamDemo.java
@@ -7,13 +7,16 @@ import java.util.stream.Collectors;
 
 public class StreamDemo {
 
+    private static final List<StreamBean> streamBeanList = List.of(new StreamBean("Bob", 18, List.of("游泳")),
+            new StreamBean("Mark", 18, List.of("游泳")),
+            new StreamBean("Anna", 19, List.of("游泳")),
+            new StreamBean("Tom", 21, List.of("游泳")));
+
     public static void main(String[] args) {
 
-        List<StreamBean> streamBeanList = List.of(new StreamBean("Bob", 18, List.of("游泳")),
-                new StreamBean("Mark", 18, List.of("游泳")),
-                new StreamBean("Anna", 19, List.of("游泳")),
-                new StreamBean("Tom", 21, List.of("游泳")));
+    }
 
+    private static void groupingBy() {
         // 分组返回指定类型 map
         LinkedHashMap<Integer, List<StreamBean>> map = streamBeanList.stream()
                 .collect(Collectors.groupingBy(StreamBean::age, LinkedHashMap::new, Collectors.toList()));
