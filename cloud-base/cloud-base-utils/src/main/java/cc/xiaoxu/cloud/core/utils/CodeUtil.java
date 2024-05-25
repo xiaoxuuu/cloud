302a2aeb5e4dfc8c9e71113064c0e56130c4e9f5
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-05-25 14:18:51 +0800

    docs: 增加注释

--- cloud-base/cloud-base-utils/src/main/java/cc/xiaoxu/cloud/core/utils/CodeUtil.java
+++ cloud-base/cloud-base-utils/src/main/java/cc/xiaoxu/cloud/core/utils/CodeUtil.java
@@ -8,22 +8,43 @@ package cc.xiaoxu.cloud.core.utils;
  */
 public class CodeUtil {
 
+    /**
+     * 获取执行代码行号
+     * @return 执行代码行号
+     */
     public static int getCurrentLineNumber() {
         return getLineNumber(2);
     }
 
+    /**
+     * 获取调用方代码行号
+     * @return 调用方代码行号
+     */
     public static int getPreviousLineNumber() {
         return getLineNumber(4);
     }
 
+    /**
+     * 获取指定栈的代码行号
+     * @param i 指定栈层数
+     * @return 对应代码行号
+     */
     private static int getLineNumber(int i) {
         return Thread.currentThread().getStackTrace()[i].getLineNumber();
     }
 
+    /**
+     * 获取执行代码信息
+     * @return 执行代码信息
+     */
     public static String getCurrentInfo() {
         return getInfo(2);
     }
 
+    /**
+     * 获取调用方信息
+     * @return 调用方信息
+     */
     public static String getPreviousInfo() {
         return getInfo(4);
     }
