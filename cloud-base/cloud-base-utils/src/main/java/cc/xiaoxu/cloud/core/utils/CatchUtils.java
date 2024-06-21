37909d7b84c3485379de071563b1de8826e3df01
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-06-21 14:22:59 +0800

    refactor: 补偿拆分

--- cloud-base/cloud-base-utils/src/main/java/cc/xiaoxu/cloud/core/utils/CatchUtils.java
+++ cloud-base/cloud-base-utils/src/main/java/cc/xiaoxu/cloud/core/utils/CatchUtils.java
@@ -12,9 +12,24 @@ import java.util.function.Supplier;
 public class CatchUtils<T> {
 
     /**
-     * 消费者
+     * 待执行代码
      */
     private final Supplier<T> trySupplier;
+
+    /**
+     * 异常补偿代码（优先）
+     */
+    private Supplier<T> orSupplier;
+
+    /**
+     * 默认数据（其次）
+     */
+    private T defaultData;
+
+    /**
+     * 空数据（兜底）
+     */
+    private final T emptyData = null;
 
     /**
      * 最终执行
@@ -46,6 +61,32 @@ public class CatchUtils<T> {
     }
 
     /**
+     * 出现异常执行的代码段
+     *
+     * @param orSupplier 待捕获代码段
+     * @return 代码执行结果
+     */
+    public CatchUtils<T> or(Supplier<T> orSupplier) {
+
+        Objects.requireNonNull(orSupplier);
+        this.orSupplier = orSupplier;
+        return this;
+    }
+
+    /**
+     * 出现异常执行的代码段
+     *
+     * @param t 默认数据
+     * @return 代码执行结果
+     */
+    public CatchUtils<T> t(T t) {
+
+        Objects.requireNonNull(t);
+        this.defaultData = t;
+        return this;
+    }
+
+    /**
      * finally 执行语句，无论是否出现异常，均会执行此代码
      *
      * @param finalRunnable 最终执行
@@ -58,21 +99,23 @@ public class CatchUtils<T> {
     }
 
     /**
-     * 执行代码，如果出现异常，执行 catchSupplier
+     * 执行代码，如果出现异常，执行 orSupplier，或返回 指定数据或 null
      *
-     * @param catchSupplier 出现异常后的处理代码
      * @return 结果
      */
-    public T handle(Supplier<T> catchSupplier) {
+    public T handle() {
         try {
             return trySupplier.get();
         } catch (Exception e) {
             e.printStackTrace();
-            return catchSupplier.get();
+            if (null != orSupplier) {
+                return orSupplier.get();
+            }
         } finally {
             if (null != finalRunnable) {
                 finalRunnable.run();
             }
         }
+        return null == defaultData ? emptyData : defaultData;
     }
 }
