ad6811a4baf59041af8425252fef8eb683c67069
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-07-03 15:23:13 +0800

    feat: fake demo

---
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
@@ -0,0 +1,15 @@
+package cc.xiaoxu.cloud.api.demo.utils_demo;
+
+import cc.xiaoxu.cloud.core.utils.random.FakePerson;
+
+import java.util.List;
+
+public class FakePersonDemo {
+
+    public static void main(String[] args) {
+
+        FakePerson.Control control = FakePerson.Control.of().gender(FakePerson.Gender.MAN);
+        List<FakePerson.Person> some = FakePerson.of().control(control).getSome(100);
+        System.out.println();
+    }
+}
