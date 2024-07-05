1acf9de43d2b01bb37f65baed546f47198d537f5
小徐 <xuhaoran@live.com>
2024-07-05 15:30:27 +0800

    refactor: 重命名

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
@@ -1,6 +1,6 @@
 package cc.xiaoxu.cloud.api.demo.utils_demo;
 
-import cc.xiaoxu.cloud.core.utils.random.person.FakePerson;
+import cc.xiaoxu.cloud.core.utils.random.person.PersonInitializer;
 
 import java.util.List;
 
@@ -8,9 +8,9 @@ public class FakePersonDemo {
 
     public static void main(String[] args) {
 
-        FakePerson.Control control = FakePerson.Control.of().gender(FakePerson.Gender.MAN);
-        List<FakePerson.Person> some = FakePerson.of().control(control).getSome(100);
-        List<FakePerson.Person> random = FakePerson.of().getSome(100);
+        PersonInitializer.Control control = PersonInitializer.Control.of().gender(PersonInitializer.Gender.MAN);
+        List<PersonInitializer.Person> some = PersonInitializer.of().control(control).getSome(100);
+        List<PersonInitializer.Person> random = PersonInitializer.of().getSome(100);
         System.out.println();
     }
 }
