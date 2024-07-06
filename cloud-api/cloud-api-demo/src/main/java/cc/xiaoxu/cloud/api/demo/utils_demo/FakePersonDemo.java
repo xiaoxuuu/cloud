90d067394271187cc565bb6a8b122cedb66527aa
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-07-06 15:31:10 +0800

    refactor: demo

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
@@ -1,6 +1,9 @@
 package cc.xiaoxu.cloud.api.demo.utils_demo;
 
+import cc.xiaoxu.cloud.core.utils.random.person.Person;
+import cc.xiaoxu.cloud.core.utils.random.person.PersonControl;
 import cc.xiaoxu.cloud.core.utils.random.person.PersonInitializer;
+import cc.xiaoxu.cloud.core.utils.random.person.enums.Gender;
 
 import java.util.List;
 
@@ -8,9 +11,9 @@ public class FakePersonDemo {
 
     public static void main(String[] args) {
 
-        PersonInitializer.Control control = PersonInitializer.Control.of().gender(PersonInitializer.Gender.MAN);
-        List<PersonInitializer.Person> some = PersonInitializer.of().control(control).getSome(100);
-        List<PersonInitializer.Person> random = PersonInitializer.of().getSome(100);
+        PersonControl control = PersonControl.of().gender(Gender.MAN);
+        List<Person> some = PersonInitializer.of().control(control).getSome(100);
+        List<Person> random = PersonInitializer.of().getSome(100);
         System.out.println();
     }
 }
