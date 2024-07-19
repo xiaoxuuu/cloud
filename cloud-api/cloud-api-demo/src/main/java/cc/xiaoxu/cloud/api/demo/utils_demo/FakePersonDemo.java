4d9c14296ea552eca7e301148ccda6662ae7dba6
小徐 <xuhaoran@live.com>
2024-07-19 14:20:24 +0800

    feat: name demo

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
@@ -4,6 +4,7 @@ import cc.xiaoxu.cloud.core.utils.random.person.Person;
 import cc.xiaoxu.cloud.core.utils.random.person.PersonInitializer;
 import cc.xiaoxu.cloud.core.utils.random.person.control.EmailControl;
 import cc.xiaoxu.cloud.core.utils.random.person.control.GenderControl;
+import cc.xiaoxu.cloud.core.utils.random.person.control.NameControl;
 import cc.xiaoxu.cloud.core.utils.random.person.control.PersonControl;
 import cc.xiaoxu.cloud.core.utils.random.person.enums.Gender;
 
@@ -14,6 +15,7 @@ public class FakePersonDemo {
     public static void main(String[] args) {
 
         PersonControl control = PersonControl.of()
+                .name(NameControl.of().useSurname(false).useDoubleSurname(true).desensitizationLength(2))
                 .gender(GenderControl.of().gender(Gender.RANDOM))
                 .email(EmailControl.of().min(1).max(2).onlyLetter(true))
                 .check();
