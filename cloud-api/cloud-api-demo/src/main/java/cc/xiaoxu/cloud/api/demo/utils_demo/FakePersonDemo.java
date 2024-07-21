b2ecb9da6d889d9bfdaadc993761a9d3a3c8f709
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-07-21 15:06:33 +0800

    feat: 手机号 demo

--- cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
+++ cloud-api/cloud-api-demo/src/main/java/cc/xiaoxu/cloud/api/demo/utils_demo/FakePersonDemo.java
@@ -2,11 +2,9 @@ package cc.xiaoxu.cloud.api.demo.utils_demo;
 
 import cc.xiaoxu.cloud.core.utils.random.person.Person;
 import cc.xiaoxu.cloud.core.utils.random.person.PersonInitializer;
-import cc.xiaoxu.cloud.core.utils.random.person.control.EmailControl;
-import cc.xiaoxu.cloud.core.utils.random.person.control.GenderControl;
-import cc.xiaoxu.cloud.core.utils.random.person.control.NameControl;
-import cc.xiaoxu.cloud.core.utils.random.person.control.PersonControl;
+import cc.xiaoxu.cloud.core.utils.random.person.control.*;
 import cc.xiaoxu.cloud.core.utils.random.person.enums.Gender;
+import cc.xiaoxu.cloud.core.utils.random.person.enums.PhoneOperator;
 
 import java.util.List;
 
@@ -17,6 +15,7 @@ public class FakePersonDemo {
         PersonControl control = PersonControl.of()
                 .name(NameControl.of().useSurname(false).useDoubleSurname(true).desensitizationLength(2))
                 .gender(GenderControl.of().gender(Gender.RANDOM))
+                .phone(PhoneControl.of().operator(PhoneOperator.RANDOM))
                 .email(EmailControl.of().min(1).max(2).onlyLetter(true))
                 .check();
         List<Person> some = PersonInitializer.of().control(control).getSome(100);
