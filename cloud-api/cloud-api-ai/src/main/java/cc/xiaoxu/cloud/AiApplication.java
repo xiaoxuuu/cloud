3c634b60ac6843cbdd8cd422f8158f50b96eecf5
小徐 <xuhaoran@live.com>
2024-08-10 17:19:43 +0800

    feat: 新增 ai 服务

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/AiApplication.java
@@ -0,0 +1,23 @@
+package cc.xiaoxu.cloud;
+
+import cc.xiaoxu.cloud.core.utils.GetStartInfoUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = "cc.xiaoxu.cloud")
+@MapperScan(basePackages = {"cc.xiaoxu.cloud.**.dao"})
+public class AiApplication {
+
+    public static void main(String[] args) {
+
+        // 获取 Spring Boot 上下文
+        ConfigurableApplicationContext ctx = SpringApplication.run(AiApplication.class, args);
+        log.error(GetStartInfoUtils.getLog(ctx));
+    }
+}
---
+++ cloud-bean/cloud-bean-ai/src/main/java/cc/xiaoxu/cloud/bean/ai/vo/AiALiSplitTxtPageVO.java
@@ -0,0 +1,30 @@
+package cc.xiaoxu.cloud.bean.ai.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiALiSplitTxtPageVO {
+
+    @Schema(description = "sk")
+    private String apiKey;
+
+    @Schema(description = "业务空间id")
+    private String workspaceId;
+
+    @Schema(description = "索引ID")
+    private String indexId;
+
+    @Schema(description = "文件ID")
+    private String filed;
+
+    @Schema(description = "页数")
+    private Integer pageNum;
+
+    @Schema(description = "每页文件大小")
+    private Integer pageSize;
+}
