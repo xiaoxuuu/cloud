ec4182d9d48b9f82444c6b61f6b6e13dabce6e42
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-08-16 14:17:16 +0800

    feat: 添加实体类

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/dao/Knowledge.java
@@ -0,0 +1,25 @@
+package cc.xiaoxu.cloud.dao;
+
+import cc.xiaoxu.cloud.core.bean.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("t_knowledge")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Knowledge extends BaseEntity {
+
+    @Schema(description = "名称")
+    private String name;
+
+    @Schema(description = "阿里文件id")
+    private String fileId;
+}
