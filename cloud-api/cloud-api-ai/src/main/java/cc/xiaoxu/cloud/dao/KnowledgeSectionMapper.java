67a7e713a61cd2dd67628a20ad29d1ac2fcf70af
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-08-16 14:19:55 +0800

    feat: 完善

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/dao/KnowledgeSectionMapper.java
@@ -0,0 +1,10 @@
+package cc.xiaoxu.cloud.dao;
+
+import cc.xiaoxu.cloud.entity.KnowledgeSection;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface KnowledgeSectionMapper extends BaseMapper<KnowledgeSection> {
+
+}
---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/service/KnowledgeSectionService.java
@@ -0,0 +1,13 @@
+package cc.xiaoxu.cloud.service;
+
+import cc.xiaoxu.cloud.dao.KnowledgeSectionMapper;
+import cc.xiaoxu.cloud.entity.KnowledgeSection;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KnowledgeSectionService extends ServiceImpl<KnowledgeSectionMapper, KnowledgeSection> {
+
+}
