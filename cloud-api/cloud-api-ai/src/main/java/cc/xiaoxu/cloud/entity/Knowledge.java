f41b1adf2ba27759bdddf2e66b7114d935f52913
小徐 <xuhaoran@live.com>
2024-08-16 14:17:45 +0800

    refactor: 实体类位置调整

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/entity/Knowledge.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/entity/Knowledge.java
@@ -1,4 +1,4 @@
-package cc.xiaoxu.cloud.dao;
+package cc.xiaoxu.cloud.entity;
 
 import cc.xiaoxu.cloud.core.bean.entity.BaseEntity;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -9,22 +9,17 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-import java.util.List;
-
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@TableName("t_knowledge_section")
+@TableName("t_knowledge")
 @NoArgsConstructor
 @AllArgsConstructor
-public class KnowledgeSection extends BaseEntity {
+public class Knowledge extends BaseEntity {
 
-    @Schema(description = "知识id")
-    private String knowledgeId;
+    @Schema(description = "名称")
+    private String name;
 
-    @Schema(description = "知识切片内容")
-    private String cutContent;
-
-    @Schema(description = "知识切片向量，1536 维")
-    private List<Double> embedding;
+    @Schema(description = "阿里文件id")
+    private String fileId;
 }
