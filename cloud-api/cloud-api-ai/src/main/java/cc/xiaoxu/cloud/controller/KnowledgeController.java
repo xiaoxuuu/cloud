5fd2527ac85fee9e8dfa1e36349ddd9838f7b243
小徐 <xuhaoran@live.com>
2024-08-19 16:19:15 +0800

    refactor: 代码迁移

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/controller/KnowledgeController.java
@@ -0,0 +1,16 @@
+package cc.xiaoxu.cloud.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@Tag(name = "知识切片服务")
+@RequestMapping("/knowledge")
+public class KnowledgeController {
+
+}
