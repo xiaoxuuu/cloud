b06ac6ebc0538188057da92372dc5de662b47a57
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-08-24 14:46:07 +0800

    feat: 千问流式返回

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/manager/ai/AiForQWen.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/manager/ai/AiForQWen.java
@@ -3,17 +3,27 @@ package cc.xiaoxu.cloud.manager.ai;
 import cc.xiaoxu.cloud.bean.ai.dto.AiChatMessageDTO;
 import cc.xiaoxu.cloud.bean.ai.dto.AiChatResultDTO;
 import cc.xiaoxu.cloud.bean.ai.enums.AiChatModelEnum;
+import cc.xiaoxu.cloud.bean.ai.vo.SseVO;
 import cc.xiaoxu.cloud.core.utils.bean.BeanUtils;
 import com.alibaba.dashscope.aigc.conversation.Conversation;
 import com.alibaba.dashscope.aigc.conversation.ConversationParam;
 import com.alibaba.dashscope.aigc.conversation.ConversationResult;
+import com.alibaba.dashscope.aigc.generation.Generation;
+import com.alibaba.dashscope.aigc.generation.GenerationParam;
+import com.alibaba.dashscope.aigc.generation.GenerationResult;
 import com.alibaba.dashscope.common.Message;
+import com.alibaba.dashscope.common.ResultCallback;
+import com.alibaba.dashscope.exception.ApiException;
+import com.alibaba.dashscope.exception.InputRequiredException;
+import com.alibaba.dashscope.exception.NoApiKeyException;
 import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 /**
  * 千问模型调用
@@ -22,7 +32,10 @@ import java.util.List;
 public class AiForQWen {
 
     protected static AiChatResultDTO chat(List<AiChatMessageDTO> aiChatMessageDto, String apiKey, AiChatModelEnum model, SseEmitter sseEmitter) {
-        return chatNotStream(aiChatMessageDto, apiKey, model);
+        if (null == sseEmitter) {
+            return chatNotStream(aiChatMessageDto, apiKey, model);
+        }
+        return chatStream(aiChatMessageDto, apiKey, model, sseEmitter);
     }
 
     /**
@@ -68,5 +81,86 @@ public class AiForQWen {
         String message = jsonObject.getString("message");
         dto.setStatusCode(statusCode);
         dto.setErrorMsg(message);
+    }
+
+    private static AiChatResultDTO chatStream(List<AiChatMessageDTO> aiChatMessageDto, String apiKey, AiChatModelEnum model, SseEmitter emitter) {
+
+        AiChatResultDTO resultDTO = new AiChatResultDTO();
+        try {
+
+            Generation gen = new Generation();
+            List<Message> messages = aiChatMessageDto.stream().map(k -> (Message) Message.builder().role(k.getRole()).content(k.getContent()).build()).toList();
+            GenerationParam param = buildGenerationParam(messages, model, apiKey);
+
+            emitter.send(SseVO.start());
+            // 处理回调
+            String fullContent = streamCallWithCallback(gen, param, emitter);
+            resultDTO.setResult(fullContent);
+            resultDTO.setStatusCode(200);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            resultDTO.setToken(0);
+            resultDTO.setStatusCode(400);
+            resultDTO.setErrorMsg(e.getMessage());
+        }
+        return resultDTO;
+    }
+
+    public static String streamCallWithCallback(Generation gen, GenerationParam param, SseEmitter emitter)
+            throws NoApiKeyException, ApiException, InputRequiredException, InterruptedException {
+        Semaphore semaphore = new Semaphore(0);
+        StringBuilder fullContent = new StringBuilder();
+
+        gen.streamCall(param, new ResultCallback<>() {
+            @Override
+            public void onEvent(GenerationResult message) {
+                String content = message.getOutput().getChoices().get(0).getMessage().getContent();
+                try {
+                    emitter.send(SseVO.msg(content));
+                    System.out.print(content);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                fullContent.append(content);
+            }
+
+            @Override
+            public void onError(Exception err) {
+                log.error("Exception occurred: {}", err.getMessage());
+                try {
+                    emitter.send(SseVO.end());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                emitter.complete();
+                semaphore.release();
+            }
+
+            @Override
+            public void onComplete() {
+                try {
+                    emitter.send(SseVO.end());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                emitter.complete();
+                semaphore.release();
+            }
+        });
+
+        semaphore.acquire();
+        return fullContent.toString();
+    }
+
+    private static GenerationParam buildGenerationParam(List<Message> userMsg, AiChatModelEnum model, String apiKey) {
+        return GenerationParam.builder()
+                .model(model.getCode())
+                .messages(userMsg)
+                .resultFormat(GenerationParam.ResultFormat.MESSAGE)
+                .topP(0.8)
+                .apiKey(apiKey)
+                .incrementalOutput(true)
+                .build();
     }
 }
