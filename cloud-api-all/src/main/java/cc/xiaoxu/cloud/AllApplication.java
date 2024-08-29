cdbe2e3823b2db1f5b85e2a700097ef1c0ce9ec6
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-08-29 15:28:07 +0800

    feat: 包扫描处理

--- cloud-api-all/src/main/java/cc/xiaoxu/cloud/AllApplication.java
+++ cloud-api-all/src/main/java/cc/xiaoxu/cloud/AllApplication.java
@@ -1,6 +1,7 @@
 package cc.xiaoxu.cloud;
 
 import cc.xiaoxu.cloud.core.utils.GetStartInfoUtils;
+import cc.xiaoxu.cloud.my.task.scheduled.WebsiteCheckScheduled;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,13 +11,18 @@ import org.springframework.context.annotation.FilterType;
 
 @Slf4j
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = {SpringBootApplication.class}))
-@SpringBootApplication(scanBasePackages = "cc.xiaoxu.cloud")
+@SpringBootApplication
+@ComponentScan(basePackages = "cc.xiaoxu.cloud")
 public class AllApplication {
 
     public static void main(String[] args) {
 
         // 获取 Spring Boot 上下文
         ConfigurableApplicationContext ctx = SpringApplication.run(AllApplication.class, args);
+
+        WebsiteCheckScheduled websiteCheckScheduled = ctx.getBean(WebsiteCheckScheduled.class);
+        websiteCheckScheduled.refreshData();
+
         log.error(GetStartInfoUtils.getLog(ctx));
     }
 }
