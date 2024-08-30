9945407c339bb4a78626e19619d614fbd5b5916e
小徐 <xuhaoran@live.com>
2024-08-30 16:20:24 +0800

    perf: 表结构调整

--- cloud-api/cloud-api-ai/table.sql
+++ cloud-api/cloud-api-ai/table.sql
@@ -1,19 +1,21 @@
 CREATE TABLE t_knowledge
 (
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(32),
-    file_id     VARCHAR(64),
-    state       VARCHAR(1) NOT NULL,
-    remark      VARCHAR(16),
-    create_time TIMESTAMP NOT NULL,
-    create_id   INT,
-    modify_time TIMESTAMP,
-    modify_id   INT
+    id              SERIAL PRIMARY KEY,
+    type            VARCHAR(16),
+    name            VARCHAR(32),
+    additional_info VARCHAR(64),
+    state           VARCHAR(1) NOT NULL,
+    remark          VARCHAR(16),
+    create_time     TIMESTAMP  NOT NULL,
+    create_id       INT,
+    modify_time     TIMESTAMP,
+    modify_id       INT
 );
-COMMENT ON TABLE t_knowledge IS '知识表';
+COMMENT ON TABLE t_knowledge IS '知识库表';
 COMMENT ON COLUMN t_knowledge.id IS '主键';
+COMMENT ON COLUMN t_knowledge.type IS '资源类型：文件、数据表、自定义分类';
 COMMENT ON COLUMN t_knowledge.name IS '名称';
-COMMENT ON COLUMN t_knowledge.file_id IS '阿里文件id';
+COMMENT ON COLUMN t_knowledge.additional_info IS '资源附加信息';
 COMMENT ON COLUMN t_knowledge.state IS '状态';
 COMMENT ON COLUMN t_knowledge.remark IS '状态';
 COMMENT ON COLUMN t_knowledge.create_time IS '创建时间';
@@ -34,7 +36,7 @@ CREATE TABLE t_knowledge_section
     modify_time  TIMESTAMP,
     modify_id    INT
 );
-COMMENT ON TABLE t_knowledge_section IS '知识表切片数据';
+COMMENT ON TABLE t_knowledge_section IS '知识库数据切片';
 COMMENT ON COLUMN t_knowledge_section.id IS '主键';
 COMMENT ON COLUMN t_knowledge_section.knowledge_id IS '知识id';
 COMMENT ON COLUMN t_knowledge_section.cut_content IS '知识切片内容';
