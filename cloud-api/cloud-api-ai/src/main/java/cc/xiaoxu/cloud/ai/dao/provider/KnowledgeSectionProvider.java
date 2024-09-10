1114f3117ff5993b2928ff47c86fa584dbf1e6f1
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-09-10 10:34:42 +0800

    feat: 不查询删除数据

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/dao/provider/KnowledgeSectionProvider.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/dao/provider/KnowledgeSectionProvider.java
@@ -2,6 +2,7 @@ package cc.xiaoxu.cloud.ai.dao.provider;
 
 import cc.xiaoxu.cloud.ai.entity.KnowledgeSection;
 import cc.xiaoxu.cloud.bean.ai.dto.AskDTO;
+import cc.xiaoxu.cloud.bean.enums.StateEnum;
 import cc.xiaoxu.cloud.core.dao.BaseProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.annotations.Param;
@@ -27,6 +28,7 @@ public class KnowledgeSectionProvider extends BaseProvider<KnowledgeSection> {
                 WHERE("knowledge_id IN (" + String.join(",", askDTO.getKnowledgeId().split(",")) + " )");
             }
             WHERE("embedding <=> " + embeddingStr + " < " + askDTO.getSimilarity());
+            WHERE("state = " + StateEnum.ENABLE.getCode() + "'");
             ORDER_BY("embedding <=> " + embeddingStr + " ASC ");
             LIMIT(askDTO.getSimilarityContentNum());
         }};
