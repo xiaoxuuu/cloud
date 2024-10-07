88fbf2590aa9fec2e93fa14b3529b075ffc3150a
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-10-07 11:45:57 +0800

    feat: 切片处理

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/task/ALiFileStatusCheckTask.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/task/ALiFileStatusCheckTask.java
@@ -15,7 +15,6 @@ import cc.xiaoxu.cloud.core.cache.CacheService;
 import cc.xiaoxu.cloud.core.utils.set.ListUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -81,35 +80,50 @@ public class ALiFileStatusCheckTask {
     /**
      * 阿里文件切片状态检查
      */
-    @Scheduled(cron = "15/45 * * * * ?")
     public void aLiFileIndexResultCheck() throws InterruptedException {
 
-        List<Knowledge> knowledgeList = knowledgeService.lambdaQuery()
-                .in(Knowledge::getStatus, List.of(ALiFileIndexResultEnum.PENDING.getCode(), ALiFileIndexResultEnum.RUNNING.getCode()))
-                .eq(Knowledge::getType, KnowledgeTypeEnum.ALi_FILE.getCode())
-                .isNotNull(Knowledge::getThreePartyInfo)
-                .list();
+        List<Knowledge> knowledgeList = getFileIndexProcessList();
         List<List<Knowledge>> lists = ListUtils.splitList(knowledgeList, 5);
         for (List<Knowledge> list : lists) {
             for (Knowledge knowledge : list) {
-                boolean succeeded = knowledgeService.updateFileIndexResult(knowledge);
-                if (!succeeded) {
-                    continue;
-                }
-
-                knowledge.setStatus(FileStatusEnum.SECTION_READ.getCode());
-                knowledgeService.updateById(knowledge);
-                knowledgeSectionService.readALiSection(knowledge);
-
-                knowledge.setStatus(FileStatusEnum.VECTOR_CALC.getCode());
-                knowledgeService.updateById(knowledge);
-                knowledgeSectionService.calcVector(new IdDTO(knowledge.getId()));
-
-                knowledge.setState(StateEnum.ENABLE.getCode());
-                knowledge.setStatus(FileStatusEnum.ALL_COMPLETED.getCode());
-                knowledgeService.updateById(knowledge);
+                aLiFileIndexResultCheck(knowledge);
             }
             Thread.sleep(1500);
         }
+    }
+
+    public void aLiFileIndexResultCheck(String knowledgeId) {
+
+        Knowledge knowledge = knowledgeService.lambdaQuery()
+                .eq(Knowledge::getId, knowledgeId)
+                .one();
+        aLiFileIndexResultCheck(knowledge);
+    }
+
+    private void aLiFileIndexResultCheck(Knowledge knowledge) {
+        boolean succeeded = knowledgeService.updateFileIndexResult(knowledge);
+        if (!succeeded) {
+            return;
+        }
+
+        knowledge.setStatus(FileStatusEnum.SECTION_READ.getCode());
+        knowledgeService.updateById(knowledge);
+        knowledgeSectionService.readALiSection(knowledge);
+
+        knowledge.setStatus(FileStatusEnum.VECTOR_CALC.getCode());
+        knowledgeService.updateById(knowledge);
+        knowledgeSectionService.calcVector(new IdDTO(knowledge.getId()));
+
+        knowledge.setState(StateEnum.ENABLE.getCode());
+        knowledge.setStatus(FileStatusEnum.ALL_COMPLETED.getCode());
+        knowledgeService.updateById(knowledge);
+    }
+
+    private List<Knowledge> getFileIndexProcessList() {
+        return knowledgeService.lambdaQuery()
+                .notIn(Knowledge::getStatus, List.of(ALiFileIndexResultEnum.COMPLETED.getCode(), ALiFileIndexResultEnum.FAILED.getCode()))
+                .eq(Knowledge::getType, KnowledgeTypeEnum.ALi_FILE.getCode())
+                .isNotNull(Knowledge::getThreePartyInfo)
+                .list();
     }
 }
