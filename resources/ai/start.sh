5e4539d447a3cad32c3f26a253f16c1465959a19
小徐 <xuhaoran@live.com>
2024-10-08 11:31:59 +0800

    feat: 脚本调整

--- resources/ai/start.sh
+++ resources/ai/start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
+
 # 找到 Conda 的 activate 脚本的路径
 ACTIVATE_SCRIPT="$HOME/anaconda3/bin/activate"
 
 # 激活 Conda 环境
-source $ACTIVATE_SCRIPT ai
+$ACTIVATE_SCRIPT ai
 
 # 在后台运行 Python 脚本
-python run.py &
+python3 run.py >> logs.txt 2>&1 &
+
+tail -f --100 logs.txt
