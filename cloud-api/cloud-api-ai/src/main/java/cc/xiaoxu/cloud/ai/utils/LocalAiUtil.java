60f997b8aa11cb2d18ef4f7b9404b08549cb263d
小徐 <xuhaoran@live.com>
2024-11-05 15:45:53 +0800

    feat: 接口位置调整

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/utils/LocalAiUtil.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/utils/LocalAiUtil.java
@@ -10,18 +10,26 @@ import java.util.List;
 
 public class LocalAiUtil {
 
-    public static final String URL = "http://192.168.5.54:55555";
-    private static final String SPLIT_BODY = """
+    public static final String URL = "http://192.168.5.20:55555";
+    private static final String vector_BODY = """
             {
                 "texts": %s,
                 "truncate_dim": 1024
             }
             """;
 
+    private static final String splitBody = """
+            {
+                "text": "%s",
+                "chunk_size": 768,
+                "chunk_overlap": 0
+            }
+            """;
+
     public static List<LocalVectorDTO> localVector(List<String> contentList) {
 
         List<LocalVectorDTO> vectorList;
-        String formatted = SPLIT_BODY.formatted(JsonUtils.toString(contentList));
+        String formatted = vector_BODY.formatted(JsonUtils.toString(contentList));
         try (Response response = OkHttpUtils.builder()
                 .url(URL + "/embeddings")
                 .body(formatted)
@@ -38,5 +46,22 @@ public class LocalAiUtil {
     public static List<Double> vector(String text) {
 
         return localVector(List.of(text)).getFirst().getEmbedding();
+    }
+
+
+    public static List<String> split(String content) {
+        List<String> textList;
+        String formatted = splitBody.formatted(content.replace(System.lineSeparator(), ""));
+        try (Response response = OkHttpUtils.builder()
+                .url(URL + "/split")
+                .body(formatted)
+                .post(true)
+                .syncResponse()) {
+            String resultData = response.body().string();
+            textList = JsonUtils.parseArray(resultData, String.class);
+        } catch (Exception e) {
+            throw new CustomException(e.getMessage());
+        }
+        return textList;
     }
 }
