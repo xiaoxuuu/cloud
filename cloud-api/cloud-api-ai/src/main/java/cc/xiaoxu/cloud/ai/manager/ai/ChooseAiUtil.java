16cbcd55470c3a86b3984f51218c4091eeccca8c
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-11-12 17:04:38 +0800

    feat: 本地 AI 测试

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/manager/ai/ChooseAiUtil.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/manager/ai/ChooseAiUtil.java
@@ -1,11 +1,16 @@
 package cc.xiaoxu.cloud.ai.manager.ai;
 
+import cc.xiaoxu.cloud.ai.utils.OkHttpUtils;
 import cc.xiaoxu.cloud.bean.ai.dto.AiChatMessageDTO;
 import cc.xiaoxu.cloud.bean.ai.dto.AiChatResultDTO;
 import cc.xiaoxu.cloud.bean.ai.enums.AiChatModelEnum;
 import cc.xiaoxu.cloud.core.exception.CustomException;
+import cc.xiaoxu.cloud.core.utils.PrettifyDateTime;
+import com.alibaba.fastjson2.JSONObject;
+import okhttp3.Response;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ChooseAiUtil {
@@ -18,7 +23,33 @@ public class ChooseAiUtil {
             case TEST, CUSTOM -> AiForTest.test(dtoList);
             case Q_WEN_72B_CHAT, Q_WEN_MAX, Q_WEN_7B_CHAT, Q_WEN_LONG_CHAT ->
                     AiForQWen.chat(dtoList, key, model, emitter);
+            case LOCAL -> AiForLocal.chat(dtoList, key, model, emitter);
             default -> throw new CustomException("暂不支持的类型：" + model.getCode());
         };
+    }
+
+    public static void main(String[] args) throws IOException {
+        Response object = OkHttpUtils.builder()
+                .url("http://172.168.1.216:8000/v1/chat/completions")
+                .body("{ \"model\": \"qwen\", \"message\": [ { \"role\": \"user\", \"content\": \"你是谁\"  } ] , \"stream\": false }")
+                .post(true)
+                .syncResponse();
+        System.out.println(JSONObject.toJSONString(object));
+
+
+        long currentTimeMillis = System.currentTimeMillis();
+        List<AiChatMessageDTO> ask = Prompt.Test.simple("你是谁");
+//        AiChatResultDTO aiChatResultDTO = ChooseAiUtil.getAiChatResultDTO(ask, "123", AiChatModelEnum.LOCAL, null);
+//        System.out.println(JsonUtils.toString(aiChatResultDTO));
+        long currentTimeMillisEnd = System.currentTimeMillis();
+        System.out.println("调用完成，耗时：" + PrettifyDateTime.initWithMillisecond(currentTimeMillisEnd - currentTimeMillis).prettify());
+
+        System.out.println("stream");
+
+        AiChatResultDTO aiChatResultDTOStream = ChooseAiUtil.getAiChatResultDTO(ask, "123", AiChatModelEnum.LOCAL, new SseEmitter());
+        long currentTimeMillisEndStream = System.currentTimeMillis();
+        System.out.println("stream 调用完成，耗时：" + PrettifyDateTime.initWithMillisecond(currentTimeMillisEndStream - currentTimeMillisEnd).prettify());
+
+        System.out.println("stream End");
     }
 }
