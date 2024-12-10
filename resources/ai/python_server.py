e7d9b2e66e039f9bf1241b54b85c14dec7e22617
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2024-12-10 15:58:09 +0800

    feat: 重构 split

--- resources/ai/python_server.py
+++ resources/ai/python_server.py
@@ -83,6 +83,36 @@ def get_embeddings():
         return jsonify({"error": str(e)}), 500
 
 
+@app.route('/split', methods=['POST'])
+def split_text_api():
+    # 获取请求中的JSON数据
+    data = request.get_json()
+
+    # 检查是否包含'text'键
+    if 'text' not in data:
+        return jsonify({'error': 'Missing text parameter'}), 400
+
+    # 获取文本
+    text = data['text']
+    chunk_size = data.get('chunk_size', 100)  # 默认值100
+    chunk_overlap = data.get('chunk_overlap', 0)  # 默认值0
+
+    # 调用分割函数
+    text_splitter = ChineseRecursiveTextSplitter(
+        # 指定在分割后的文本中是否保留分隔符，默认为 True
+        keep_separator=True,
+        # 指定分隔符列表中的元素是否为正则表达式，默认为 True
+        is_separator_regex=True,
+        # 每个块的最大大小，大小由 length_function 决定。
+        chunk_size=chunk_size,
+        # 块之间的目标重叠。重叠的块有助于减轻在块之间划分上下文时信息的丢失。
+        chunk_overlap=chunk_overlap
+        # length_function：确定块大小的函数，默认为 len，即按字符数计算。
+    )
+    split_results = text_splitter.split_text(text)
+
+    # 返回结果
+    return jsonify(split_results)
 
 
 if __name__ == '__main__':
