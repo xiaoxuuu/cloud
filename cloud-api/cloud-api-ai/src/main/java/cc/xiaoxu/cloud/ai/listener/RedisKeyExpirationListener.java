858d6effe7c77bf5ff2d4f21c1af55ad507e27da
小徐 <xuhaoran@live.com>
2025-01-02 10:12:35 +0800

    feat: 移除 ali 相关代码

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/listener/RedisKeyExpirationListener.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/listener/RedisKeyExpirationListener.java
@@ -1,8 +1,6 @@
 package cc.xiaoxu.cloud.ai.listener;
 
 import cc.xiaoxu.cloud.ai.constants.RedisListenerConstants;
-import cc.xiaoxu.cloud.ai.task.ALiFileStatusCheckTask;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
@@ -17,9 +15,6 @@ public class RedisKeyExpirationListener extends KeyExpirationEventMessageListene
     public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
         super(listenerContainer);
     }
-
-    @Resource
-    private ALiFileStatusCheckTask aLiFileStatusCheckTask;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
@@ -37,22 +32,6 @@ public class RedisKeyExpirationListener extends KeyExpirationEventMessageListene
 
         if (!expireKey.startsWith(RedisListenerConstants.PREFIX)) {
             return;
-        }
-
-        // 数据检查
-        switch (expireKey) {
-            case RedisListenerConstants.FILE_UPLOAD_RESULT_CHECK -> aLiFileStatusCheckTask.aLiFileUploadResultCheck();
-            case RedisListenerConstants.FILE_INDEX_RESULT_CHECK -> aLiFileStatusCheckTask.aLiFileIndexResultCheck();
-        }
-
-        // 文件上传结果
-        if (expireKey.startsWith(RedisListenerConstants.FILE_UPLOAD_RESULT_HANDLE)) {
-            aLiFileStatusCheckTask.aLiFileUploadResultCheck(expireKey.replace(RedisListenerConstants.FILE_UPLOAD_RESULT_HANDLE, ""));
-        }
-
-        // 文件切片结果
-        if (expireKey.startsWith(RedisListenerConstants.FILE_INDEX_RESULT_HANDLE)) {
-            aLiFileStatusCheckTask.aLiFileIndexResultCheck(expireKey.replace(RedisListenerConstants.FILE_INDEX_RESULT_HANDLE, ""));
         }
     }
 }
--- cloud-bean/cloud-bean-ai/src/main/java/cc/xiaoxu/cloud/bean/ai/enums/AiChatModelEnum.java
+++ cloud-bean/cloud-bean-ai/src/main/java/cc/xiaoxu/cloud/bean/ai/enums/AiChatModelEnum.java
@@ -12,12 +12,6 @@ public enum AiChatModelEnum implements EnumInterface<String>, EnumDescInterface 
     // KIMI
     MOONSHOT_V1_128K("moonshot-v1-128k", "KIMI 128K", AiTypeEnum.KIMI),
 
-    // 千问
-    Q_WEN_MAX("qwen-max", "千问 MAX", AiTypeEnum.Q_WEN),
-    Q_WEN_72B_CHAT("qwen-72b-chat", "千问 72B", AiTypeEnum.Q_WEN),
-    Q_WEN_7B_CHAT("qwen1.5-7b-chat", "千问 7B", AiTypeEnum.Q_WEN),
-    Q_WEN_LONG_CHAT("qwen-long", "千问 LONG", AiTypeEnum.Q_WEN),
-
     // 本地
     LOCAL("qwen", "本地 千问 32B", AiTypeEnum.Q_WEN),
     LOCAL_QWEN2_5_32B_INSTRUCT_AWQ("Qwen2.5-32B-Instruct-AWQ", "本地 千问2.5 32B", AiTypeEnum.Q_WEN),
