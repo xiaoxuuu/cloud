c5d7ef771bf3714808b8371138720f0aac1d1224
小徐 <xuhaoran@live.com>
2025-01-09 14:56:35 +0800

    feat: ConversationDetailContent

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/service/ConversationDetailContentService.java
@@ -0,0 +1,15 @@
+package cc.xiaoxu.cloud.ai.service;
+
+import cc.xiaoxu.cloud.ai.dao.ConversationDetailContentMapper;
+import cc.xiaoxu.cloud.ai.entity.ConversationDetailContent;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ConversationDetailContentService extends ServiceImpl<ConversationDetailContentMapper, ConversationDetailContent> {
+
+}
