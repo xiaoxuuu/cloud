e7120c1a49e6bff188814e6a93f4742091fa5f09
小徐 <xuhaoran@live.com>
2025-01-09 14:56:04 +0800

    feat: ConversationDetail

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/dao/ConversationDetailMapper.java
@@ -0,0 +1,10 @@
+package cc.xiaoxu.cloud.ai.dao;
+
+import cc.xiaoxu.cloud.ai.entity.ConversationDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ConversationDetailMapper extends BaseMapper<ConversationDetail> {
+
+}
