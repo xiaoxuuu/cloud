1894650f6167c2c8719acc4009c3d8522489422b
小徐 <xuhaoran@live.com>
2025-01-22 14:56:43 +0800

    feat: 聊天记忆

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/manager/PersistentChatMemoryStore.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/manager/PersistentChatMemoryStore.java
@@ -1,11 +1,9 @@
 package cc.xiaoxu.cloud.ai.manager;
 
-import cc.xiaoxu.cloud.ai.entity.Conversation;
 import cc.xiaoxu.cloud.ai.service.ConversationDetailService;
 import cc.xiaoxu.cloud.ai.service.ConversationService;
 import cc.xiaoxu.cloud.bean.ai.enums.AiChatRoleEnum;
 import cc.xiaoxu.cloud.bean.ai.vo.ConversationDetailVO;
-import cc.xiaoxu.cloud.bean.enums.StateEnum;
 import cc.xiaoxu.cloud.core.utils.enums.EnumUtils;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
@@ -13,10 +11,12 @@ import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 public class PersistentChatMemoryStore implements ChatMemoryStore {
 
@@ -29,6 +29,7 @@ public class PersistentChatMemoryStore implements ChatMemoryStore {
     @Override
     public List<ChatMessage> getMessages(Object memoryId) {
 
+        log.error("getMessages");
         List<ConversationDetailVO> conversationDetailList = conversationDetailService.getConversationDetailList((int) memoryId);
         return conversationDetailList.stream().map(k -> {
             AiChatRoleEnum roleEnum = EnumUtils.getByClass(k.getModel(), AiChatRoleEnum.class);
@@ -43,14 +44,16 @@ public class PersistentChatMemoryStore implements ChatMemoryStore {
     @Override
     public void updateMessages(Object memoryId, List<ChatMessage> messages) {
 
+        log.error("updateMessages");
     }
 
     @Override
     public void deleteMessages(Object memoryId) {
 
-        conversationService.lambdaUpdate()
-                .set(Conversation::getState, StateEnum.DELETE.getCode())
-                .eq(Conversation::getId, memoryId)
-                .update();
+        log.error("deleteMessages");
+//        conversationService.lambdaUpdate()
+//                .set(Conversation::getState, StateEnum.DELETE.getCode())
+//                .eq(Conversation::getId, memoryId)
+//                .update();
     }
 }
