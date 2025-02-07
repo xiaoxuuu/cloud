4bee793db162901f1a6d59acc7e1164056831850
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2025-02-07 15:59:05 +0800

    feat: KnowledgeBase

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/entity/KnowledgeBase.java
@@ -0,0 +1,26 @@
+package cc.xiaoxu.cloud.ai.entity;
+
+import cc.xiaoxu.cloud.core.bean.entity.BaseEntityForPostgres;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_knowledge_base")
+@Schema(description = "知识库")
+public class KnowledgeBase extends BaseEntityForPostgres {
+
+    @Schema(description = "名称")
+    private Integer conversationId;
+
+    @Schema(description = "知识库id")
+    private Integer knowledgeBaseId;
+}
