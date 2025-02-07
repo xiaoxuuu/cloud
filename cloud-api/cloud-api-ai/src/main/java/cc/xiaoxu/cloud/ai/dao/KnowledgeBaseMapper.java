d72a343664bd9a300ff92fd5be0580523e0c26d2
小徐 <xuhaoran@live.com>
2025-02-07 15:59:05 +0800

    feat: KnowledgeBase

---
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/dao/KnowledgeBaseMapper.java
@@ -0,0 +1,10 @@
+package cc.xiaoxu.cloud.ai.dao;
+
+import cc.xiaoxu.cloud.ai.entity.KnowledgeBase;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface KnowledgeBaseMapper extends BaseMapper<KnowledgeBase> {
+
+}
