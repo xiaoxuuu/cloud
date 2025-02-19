67914834658c72ce889c0394b269ac6e195a644a
小徐 <x@xiaoxu.cc>
2025-02-19 13:57:25 +0800

    feat: 对话详情

--- cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/controller/ConversationDetailController.java
+++ cloud-api/cloud-api-ai/src/main/java/cc/xiaoxu/cloud/ai/controller/ConversationDetailController.java
@@ -1,8 +1,17 @@
 package cc.xiaoxu.cloud.ai.controller;
 
+import cc.xiaoxu.cloud.ai.service.ConversationDetailService;
+import cc.xiaoxu.cloud.ai.utils.UserUtils;
+import cc.xiaoxu.cloud.bean.ai.dto.ConversationDetailPageDTO;
+import cc.xiaoxu.cloud.bean.ai.vo.ConversationDetailVO;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/conversation")
 public class ConversationDetailController {
 
-    // TODO 详情查询
+    private final ConversationDetailService conversationDetailService;
+
+    @PostMapping("/page")
+    @Operation(summary = "分页")
+    public Page<ConversationDetailVO> page(@Valid @RequestBody ConversationDetailPageDTO dto) {
+
+        return conversationDetailService.pages(dto, UserUtils.getUserId());
+    }
 }
