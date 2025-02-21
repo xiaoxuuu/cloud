a44cf4280c8fe4f6d6f662ea4de1e134b6b36d72
小徐 <x@xiaoxu.cc>
2025-02-21 10:35:37 +0800

    feat: truncate_dim

--- resources/ai/python_server.py
+++ resources/ai/python_server.py
@@ -41,9 +41,10 @@ async def get_embeddings(request: EmbeddingsRequest):
     """处理文本嵌入请求"""
     try:
         texts = request.input
+        truncate_dim = 1024
 
         # 为每个文本生成嵌入向量
-        embeddings = embedding_model.encode(texts, task="text-matching", 1024)
+        embeddings = embedding_model.encode(texts, task="text-matching", truncate_dim=truncate_dim)
 
         # 返回嵌入向量
         return [{"index": i, "text": text, "embedding": embedding.tolist()}
