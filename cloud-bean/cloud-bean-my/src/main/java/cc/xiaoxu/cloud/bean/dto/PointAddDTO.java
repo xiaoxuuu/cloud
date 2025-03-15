5c833f788cff25ef4d4b0e91e8ba5aabbe4b6e6b
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2025-03-15 20:41:17 +0800

    feat: 点位新增

--- cloud-bean/cloud-bean-my/src/main/java/cc/xiaoxu/cloud/bean/dto/PointAddDTO.java
+++ cloud-bean/cloud-bean-my/src/main/java/cc/xiaoxu/cloud/bean/dto/PointAddDTO.java
@@ -1,7 +1,6 @@
 package cc.xiaoxu.cloud.bean.dto;
 
 import cc.xiaoxu.cloud.bean.enums.PointTypeEnum;
-import com.baomidou.mybatisplus.annotation.TableField;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,7 +13,6 @@ import lombok.NoArgsConstructor;
 public class PointAddDTO {
 
     @Schema(description = "点位类型")
-    @TableField(value = "point_type")
     private PointTypeEnum pointType;
 
     @Schema(description = "名称")
@@ -23,6 +21,39 @@ public class PointAddDTO {
     @Schema(description = "地点描述")
     private String describe;
 
+    @Schema(description = "详细地址")
+    private String address;
+
+    @Schema(description = "经度")
+    private String longitude;
+
+    @Schema(description = "纬度")
+    private String latitude;
+
+    @Schema(description = "收藏次数")
+    private Integer collectTimes;
+
+    @Schema(description = "我去过的次数")
+    private Integer visitedTimes;
+
     @Schema(description = "来源")
     private String source;
+
+    @Schema(description = "地址code")
+    private String addressCode;
+
+    @Schema(description = "高德 - WIA 坐标（来源高德小程序）")
+    private String amapWia;
+
+    @Schema(description = "高德 - 标签")
+    private String amapTag;
+
+    @Schema(description = "高德 - 评分")
+    private String amapRating;
+
+    @Schema(description = "高德 - 人均消费")
+    private String amapCost;
+
+    @Schema(description = "高德 - POI ID")
+    private String amapPoiId;
 }
