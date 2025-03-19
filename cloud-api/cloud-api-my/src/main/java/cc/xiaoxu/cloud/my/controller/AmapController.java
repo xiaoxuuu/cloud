e3608c34e5b48fc147a7b4f5a8c024eb09c2c002
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2025-03-19 13:35:49 +0800

    feat: code 引入

--- cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/controller/AmapController.java
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/controller/AmapController.java
@@ -1,36 +1,35 @@
 package cc.xiaoxu.cloud.my.controller;
 
-import cc.xiaoxu.cloud.bean.dto.amap.AmapInputTipsRequestDTO;
-import cc.xiaoxu.cloud.bean.dto.amap.AmapInputTipsResponseDTO;
 import cc.xiaoxu.cloud.bean.dto.amap.AmapPoiSearchRequestDTO;
 import cc.xiaoxu.cloud.bean.dto.amap.AmapPoiSearchResponseDTO;
+import cc.xiaoxu.cloud.core.controller.CloudController;
+import cc.xiaoxu.cloud.core.exception.CustomException;
 import cc.xiaoxu.cloud.my.manager.AmapManager;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
 @Tag(name = "高德地图", description = "高德地图接口器")
 @RequestMapping("/amap")
 public class AmapController {
 
-    private final AmapManager amapManager;
+    @Value("${app.config.auth-code}")
+    private String authCode;
 
-    @Operation(summary = "输入提示", description = "inputTips")
-    @PostMapping("/inputTips")
-    public @ResponseBody
-    AmapInputTipsResponseDTO inputTips(@RequestBody AmapInputTipsRequestDTO dto) {
-
-        return amapManager.inputTips(dto);
-    }
+    @Resource
+    private AmapManager amapManager;
 
     @Operation(summary = "poi 搜索", description = "poi 搜索")
-    @PostMapping("/searchPoi")
+    @PostMapping("/search_poi/{code}")
     public @ResponseBody
-    AmapPoiSearchResponseDTO searchPoi(@RequestBody AmapPoiSearchRequestDTO dto) {
+    AmapPoiSearchResponseDTO searchPoi(@PathVariable("code") String code, @RequestBody AmapPoiSearchRequestDTO dto) {
 
+        if (!code.equals(CloudController.getCheckCode() + authCode)) {
+            throw new CustomException("无权限");
+        }
         dto.setShowFields("business");
         dto.setExtensions("all");
         return amapManager.searchPoi(dto);
