d1048ef73ab802f6b9b57b8795e9e41f88d329a2
小徐 <xuhaoran@live.com>
2025-03-23 22:07:20 +0800

    feat: 删除数据

--- cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/dao/PointMapper.java
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/dao/PointMapper.java
@@ -3,8 +3,13 @@ package cc.xiaoxu.cloud.my.dao;
 import cc.xiaoxu.cloud.my.entity.Point;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface PointMapper extends BaseMapper<Point> {
 
+    @Select("""
+            SELECT COUNT(1) FROM t_point WHERE "state" = 'P'
+            """)
+    Integer countProgressing();
 }
