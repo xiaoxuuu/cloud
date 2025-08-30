c3aa703e008c416b0849a65d180e2d972c587410
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2025-08-30 22:10:37 +0800

    feat: 参数变更

--- cloud-bean/cloud-bean-my/src/main/java/cc/xiaoxu/cloud/bean/dto/PointSearchDTO.java
+++ cloud-bean/cloud-bean-my/src/main/java/cc/xiaoxu/cloud/bean/dto/PointSearchDTO.java
@@ -13,14 +13,20 @@ import java.util.List;
 @Schema(description = "点位 - 搜索 - 请求参数")
 public class PointSearchDTO {
 
+    @Schema(description = "地图缩放层级", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Double scale;
+
+    @Schema(description = "中心点纬度", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Double centerLatitude;
+
+    @Schema(description = "中心点纬度", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Double centerLongitude;
+
     @Schema(description = "点位类型")
     private List<String> pointType;
 
     @Schema(description = "点位名称")
     private String pointName;
-
-    @Schema(description = "缩放层级")
-    private Double level;
 
     @Schema(description = "作者去过")
     private Boolean visit;
