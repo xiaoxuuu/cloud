f49c56d73bf66b139d747b44e835c4ba705744d6
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2025-09-25 19:54:07 +0800

    feat: 批量处理数据

--- cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/service/AreaService.java
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/service/AreaService.java
@@ -1,5 +1,6 @@
 package cc.xiaoxu.cloud.my.service;
 
+import cc.xiaoxu.cloud.bean.enums.StateEnum;
 import cc.xiaoxu.cloud.my.dao.AreaMapper;
 import cc.xiaoxu.cloud.my.entity.Area;
 import cc.xiaoxu.cloud.my.manager.AmapManager;
@@ -9,6 +10,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -21,8 +23,13 @@ public class AreaService extends ServiceImpl<AreaMapper, Area> {
 
     public void updateLocation() {
 
-        List<Area> areaList = lambdaQuery().isNull(Area::getLocation).list();
+        List<Area> areaList = lambdaQuery()
+                .isNull(Area::getLocation)
+                .ne(Area::getState, StateEnum.DELETE.getCode())
+                .list();
+        log.error("更新：" + areaList.size());
         for (Area area : areaList) {
+            log.error("更新：" + area.getName());
             String location = null;
             if ("1".equals(area.getLevel())) {
                 location = amapManager.getPoiLocation(area.getName() + "政府", null);
@@ -30,13 +37,21 @@ public class AreaService extends ServiceImpl<AreaMapper, Area> {
                 location = amapManager.getPoiLocation(area.getName() + "政府", area.getName());
             } else if ("3".equals(area.getLevel())) {
                 Area one = lambdaQuery().eq(Area::getShortCode, area.getShortCode().substring(0, 4)).one();
+                if (null == one) {
+                    lambdaUpdate()
+                            .eq(Area::getId, area.getId())
+                            .set(Area::getState, StateEnum.DELETE.getCode())
+                            .set(Area::getUpdateTime, LocalDateTime.now())
+                            .update();
+                    continue;
+                }
                 location = amapManager.getPoiLocation(one.getName() + area.getName() + "政府", one.getName());
             }
             area.setLocation(location);
             lambdaUpdate()
                     .eq(Area::getId, area.getId())
                     .set(Area::getLocation, location)
-                    .set(Area::getUpdateTime, System.currentTimeMillis())
+                    .set(Area::getUpdateTime, LocalDateTime.now())
                     .update();
         }
     }
