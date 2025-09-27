b78e0ca5d9057548d634ba094300e4df622b01a6
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2025-09-27 11:33:58 +0800

    feat: 拉取逻辑调整

--- cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/service/AreaService.java
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/service/AreaService.java
@@ -7,6 +7,7 @@ import cc.xiaoxu.cloud.my.manager.AmapManager;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -21,38 +22,65 @@ public class AreaService extends ServiceImpl<AreaMapper, Area> {
 
     private final AmapManager amapManager;
 
+    @SneakyThrows
     public void updateLocation() {
 
         List<Area> areaList = lambdaQuery()
                 .isNull(Area::getLocation)
-                .ne(Area::getState, StateEnum.DELETE.getCode())
+                .eq(Area::getState, StateEnum.ENABLE.getCode())
                 .list();
         log.error("更新：" + areaList.size());
         for (Area area : areaList) {
-            log.error("更新 " + area.getCode() + " " + area.getName());
+            Thread.sleep(1000);
             String location = null;
+            String poiName = null;
             if ("1".equals(area.getLevel())) {
-                location = amapManager.getPoiLocation(area.getName() + "政府", null);
+                poiName = area.getName() + "政府";
+                location = amapManager.getPoiLocation(poiName, null);
             } else if ("2".equals(area.getLevel())) {
-                location = amapManager.getPoiLocation(area.getName() + "政府", area.getName());
-            } else if ("3".equals(area.getLevel())) {
-                Area one = lambdaQuery().eq(Area::getShortCode, area.getShortCode().substring(0, 4)).one();
-                if (null == one) {
+                Area province = lambdaQuery().eq(Area::getShortCode, area.getShortCode().substring(0, 2)).one();
+                if (null == province) {
                     lambdaUpdate()
                             .eq(Area::getId, area.getId())
                             .set(Area::getState, StateEnum.DELETE.getCode())
                             .set(Area::getUpdateTime, LocalDateTime.now())
                             .update();
+                    log.error("未查询到省：" + poiName);
                     continue;
                 }
-                location = amapManager.getPoiLocation(one.getName() + area.getName() + "政府", one.getName());
+                poiName = province.getName() + area.getName() + "政府";
+                location = amapManager.getPoiLocation(poiName, area.getName());
+            } else if ("3".equals(area.getLevel())) {
+                Area province = lambdaQuery().eq(Area::getShortCode, area.getShortCode().substring(0, 2)).one();
+                Area city = lambdaQuery().eq(Area::getShortCode, area.getShortCode().substring(0, 4)).one();
+                if (null == city && null == province) {
+                    lambdaUpdate()
+                            .eq(Area::getId, area.getId())
+                            .set(Area::getState, StateEnum.DELETE.getCode())
+                            .set(Area::getUpdateTime, LocalDateTime.now())
+                            .update();
+                    log.error("未查询到省市：" + poiName);
+                    continue;
+                }
+                String cityName = null == city ? null : city.getName();
+                poiName = (province.getName() + cityName + area.getName() + "政府").replace("null", "");
+                location = amapManager.getPoiLocation(poiName, cityName);
             }
+            log.error("更新 " + area.getCode() + " " + poiName);
             area.setLocation(location);
-            lambdaUpdate()
-                    .eq(Area::getId, area.getId())
-                    .set(Area::getLocation, location)
-                    .set(Area::getUpdateTime, LocalDateTime.now())
-                    .update();
+            if (null == location) {
+                lambdaUpdate()
+                        .eq(Area::getId, area.getId())
+                        .set(Area::getState, StateEnum.LOCK.getCode())
+                        .set(Area::getUpdateTime, LocalDateTime.now())
+                        .update();
+            } else {
+                lambdaUpdate()
+                        .eq(Area::getId, area.getId())
+                        .set(Area::getLocation, location)
+                        .set(Area::getUpdateTime, LocalDateTime.now())
+                        .update();
+            }
         }
     }
 }
