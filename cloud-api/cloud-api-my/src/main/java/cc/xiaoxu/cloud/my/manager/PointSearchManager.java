1d7cbf973193f964ca74f5c7a93363bc758e0093
小徐 <22138287+xiaoxuuu@users.noreply.github.com>
2025-12-05 18:20:36 +0800

    feat: 默认营业状态

--- cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/manager/PointSearchManager.java
+++ cloud-api/cloud-api-my/src/main/java/cc/xiaoxu/cloud/my/manager/PointSearchManager.java
@@ -42,11 +42,9 @@ public class PointSearchManager {
         int countConstant = Integer.parseInt(count.getValue());
 
         // 营业状态
-        Set<OperatingStatusEnum> operatingStatusSet = dto.getOperatingStatusSet();
-        if (CollectionUtils.isEmpty(operatingStatusSet)) {
-            operatingStatusSet.add(OperatingStatusEnum.OPEN);
-            operatingStatusSet.add(OperatingStatusEnum.ING);
-        }
+        Set<OperatingStatusEnum> operatingStatusSet = CollectionUtils.isNotEmpty(dto.getOperatingStatusSet())
+                ? dto.getOperatingStatusSet()
+                : Set.of(OperatingStatusEnum.OPEN, OperatingStatusEnum.ING);
 
         List<PointTemp> pointFilterList = pointManager.getPointList()
                 .stream()
